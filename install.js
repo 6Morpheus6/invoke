ab7fcdae0296babfbccdf5bfe2996ad9b68e0c35
6Morpheus6 <wanton-decay@gmx.net>
2026-05-27 03:34:21 +0200

    incompatibility notification for amd on windows

--- install.js
+++ install.js
@@ -2,43 +2,56 @@ module.exports = {
   requires: {
     bundle: "ai",
   },
-  run: [{
-    method: "shell.run",
-    params: {
-      path: "app",
-        env: {
-          UV_VENV_CLEAR: "1"
-        },
-      message: [
-        "uv venv --relocatable --prompt invoke --python 3.12 --python-preference only-managed env",
-      ]
-    }
-  }, {
-    method: "script.start",
-    params: {
-      uri: "torch.js",
+  run: [
+    {
+      when: "{{platform === 'win32' && gpu === 'amd'}}",
+      method: "notify",
       params: {
-        venv: "env",
-        path: "app"
+        html: "This app is not compatible with AMD on Windows."
+      },
+      next: null
+    },
+    {
+      method: "shell.run",
+      params: {
+        path: "app",
+          env: {
+            UV_VENV_CLEAR: "1"
+          },
+        message: [
+          "uv venv --relocatable --prompt invoke --python 3.12 --python-preference only-managed env",
+        ]
+      }
+    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",
+          path: "app"
+        }
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        drive: {
+          "models": "app/models",
+          "databases": "app/databases",
+          "autoimport": "app/autoimport",
+          "outputs": "app/outputs",
+          "nodes": "app/nodes",
+          "text-inversion-output": "app/text-inversion-output",
+          "text-inversion-training-data": "app/text-inversion-training-data"
+        }
+      }
+    },
+    {
+      method: "notify",
+      params: {
+        html: "App launched. Click 'start' to get started"
       }
     }
-  }, {
-    method: "fs.link",
-    params: {
-      drive: {
-        "models": "app/models",
-        "databases": "app/databases",
-        "autoimport": "app/autoimport",
-        "outputs": "app/outputs",
-        "nodes": "app/nodes",
-        "text-inversion-output": "app/text-inversion-output",
-        "text-inversion-training-data": "app/text-inversion-training-data"
-      }
-    }
-  }, {
-    method: "notify",
-    params: {
-      html: "App launched. Click 'start' to get started"
-    }
-  }]
+  ]
 }
